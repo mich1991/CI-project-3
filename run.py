2f511c6e862755e4ef4306423b86cce48be55059
mich1991 <wozniak.michal91@gmail.com>
2022-08-21 17:32:24 +0100

    init commit + test deployment on heroku

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import inquirer
+
+difficulty = ['easy', 'medium', 'hard']
+level = inquirer.list_input('Choose difficulty level', choices=difficulty)
+
