34a36407b9703980c6af7087d1a49a57067c88db
mich1991 <wozniak.michal91@gmail.com>
2022-08-21 17:42:40 +0100

    test if inquirer library will work on heroku frontend terminal

--- run.py
+++ run.py
@@ -6,3 +6,6 @@ import inquirer
 difficulty = ['easy', 'medium', 'hard']
 level = inquirer.list_input('Choose difficulty level', choices=difficulty)
 
+name = inquirer.text(message="Enter your pymagotchi name")
+
+print(f"game difficulty is {level} and pet name is {name}")
