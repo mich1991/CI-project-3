ed22be57735210c769ceacdea21f4f667303b7ac
mich1991 <wozniak.michal91@gmail.com>
2022-08-22 20:25:24 +0100

    add game starting logic

--- run.py
+++ run.py
@@ -22,14 +22,15 @@ class Pet():
         self.hunger = 0
         self.tiredness = 0
 
-        self.foodStock = 1
-        self.isAlive = True
+        self.food_stock = 1
         self.game_difficulty = self.game_level(difficulty)
+        self.is_alive = True
+        self.round = 0
 
     def eat(self):
         """Deduct 1 from foodStock to lower hunger"""
-        if self.foodStock > 0:
-            self.foodStock -= 1
+        if self.food_stock > 0:
+            self.food_stock -= 1
             dish = random.randint(2,4)
             self.hunger -= dish
             print(f"\n{name} ate something from the fridge worth {dish} hunger points!")
@@ -37,6 +38,7 @@ class Pet():
             print(f"\nFridge is empty. Go and find some food already.")
 
     def play(self):
+        """reduce boredom value by a random int."""
         game_value = random.randint(1,4)
         if self.boredom < game_value:
             self.boredom = 0
@@ -45,17 +47,20 @@ class Pet():
         print(f"\n{name} had fun playing with you! ({game_value})")
 
     def sleep(self):
+        """Set tiredness to 0"""
         self.tiredness = 0
         print(f"{name} hit the bunk! Good night, sleep tight!")
 
-    def foodHunting(self):
-        food_found = random.randint(1,3)
-        self.foodStock += food_found
+    def food_hunting(self):
+        """Collect random number of food"""
+        food_found = random.randint(1, 3)
+        self.food_stock += food_found
         print(f"\n{name} found {food_found} food. That will keep him going for awhile")
 
 
     def bath(self):
-        bath_value = random.randint(1,4)
+        """reduce dirtiness"""
+        bath_value = random.randint(1, 4)
         if self.dirtiness < bath_value:
             self.dirtiness = 0
         else:
@@ -63,18 +68,58 @@ class Pet():
         print(f"\n{name} finally took a bath! It was worth {bath_value} points!")
 
     def time_passed(self):
+        """Increase value of every attribute at the end of each round"""
         # pet_attributes = ['boredom', 'hunger', 'dirtiness','tiredness']
         pet_attributes = [self.tiredness, self.hunger, self.boredom, self.dirtiness]
         for attr in pet_attributes:
             attr += random.randint(1, self.game_difficulty)
 
-    def game_level(self):
+    def show_current_status(self):
+        """Shows current attributes values for a pymagotchi"""
+        print(f"-----------------------------------")
+        print(f"\nRound: {self.round}  {self.name} current status :")
+        print(f"\nTiredness value : {self.tiredness} (0-20)")
+        print(f"\nHunger value : {self.hunger} (0-20)")
+        print(f"\nBoredom value : {self.boredom} (0-20)")
+        print(f"\nDirtiness value : {self.dirtiness} (0-20)")
+        print(f"\nFridge has food units: {self.food_stock} (0-infinity!)")
+        print(f"-----------------------------------")
+
+    def pet_activity_choice(self):
+        """Show menu options for player to choose current activity for pymagotchi"""
+        activities = ['play', 'eat', 'bath', 'sleep', 'food hunting']
+        choice = inquirer.list_input(f"What should {self.name} do now?", choices=activities)
+        if choice == 'play':
+            self.play()
+        elif choice == 'eat':
+            self.eat()
+        elif choice == 'bath':
+            self.bath()
+        elif choice == 'sleep':
+            self.sleep()
+        elif choice == 'food hunting':
+            self.food_hunting()
+        else:
+            print('\nHow did you do that?')
+            print('----------------------')
+            self.pet_activity_choice()
+
+
+    def game_level(self, level):
         if level == 'easy':
             return 2
         elif level == 'medium':
-            return 3
-        elif level == 'hard':
             return 4
+        elif level == 'hard':
+            return 6
         else:
             # Don't mess with me.
             return 9999
+
+
+pymagotchi = Pet(name, level)
+
+while pymagotchi.is_alive:
+    pymagotchi.pet_activity_choice()
+    pymagotchi.time_passed()
+    pymagotchi.show_current_status()
