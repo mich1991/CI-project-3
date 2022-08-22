0168eedf9ab5a8a37d73f889ab2f5239e18f03f8
mich1991 <wozniak.michal91@gmail.com>
2022-08-22 19:11:01 +0100

    define core methods for pymatochi

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import inquirer
+import random
 
 difficulty = ['easy', 'medium', 'hard']
 level = inquirer.list_input('Choose difficulty level', choices=difficulty)
@@ -9,3 +10,36 @@ level = inquirer.list_input('Choose difficulty level', choices=difficulty)
 name = inquirer.text(message="Enter your pymagotchi name")
 
 print(f"game difficulty is {level} and pet name is {name}")
+
+class Pet():
+    """Create a pymagotchi"""
+
+    def __init__(self, name):
+        self.name = name
+        self.boredom = 0
+        self.dirtiness = 0
+        self.hunger = 0
+        self.tiredness = 0
+
+        self.foodStock = 1
+        self.isAlive = True
+
+    def eat(self):
+        """Deduct 1 from foodStock to lower hunger"""
+        if self.foodStock > 0:
+            self.foodStock -= 1
+            dish = random.randint(2,5)
+            self.hunger -= dish
+            print(f"{name} ate something from the fridge worth {dish} hunger points!")
+        else:
+            print(f"Fridge is empty. Go and find some food already.")
+
+
+    def play(self):
+
+    def sleep(self):
+
+    def foodHunting(self):
+
+    def bath(self):
+
